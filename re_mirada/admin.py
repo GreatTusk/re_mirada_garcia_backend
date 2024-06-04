5af65b44c27f329a5e5191b234462a7395e58fc7
Fernando <131561675+GreatTusk@users.noreply.github.com>
2024-06-04 10:36:50 -0400

    registered some sites

--- re_mirada/admin.py
+++ re_mirada/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import (ItemPortafolio, Servicios, PlanFoto, Cliente,
+                     ItemTestimonio, ImageFolders)
 
 # Register your models here.
+admin.site.register(PlanFoto)
+admin.site.register(Cliente)
+admin.site.register(ItemTestimonio)
+admin.site.register(ItemPortafolio)
+admin.site.register(ImageFolders)
+admin.site.register(Servicios)
