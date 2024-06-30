6d07f9253138ad88722161306ad6455d3bb4bac9
F776 <fern.belmar@duocuc.cl>
2024-06-29 23:10:29 -0400

    carrito de compras listo: agregar un "ver pedido y estado de pedidos" ya es opcional

--- re_mirada_garcia_backend/settings.py
+++ re_mirada_garcia_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,8 +24,7 @@ SECRET_KEY = 'django-insecure-t^)r0=5w1ozaw*5yp0-)f!jh!-&y=50hvion*+@dkx7-cy-kej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
+ALLOWED_HOSTS = ["127.0.0.1", '.vercel.app', '.now.sh']
 
 # Application definition
 
@@ -126,8 +125,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
-
-
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- re_mirada_garcia_backend/wsgi.py
+++ re_mirada_garcia_backend/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 're_mirada_garcia_backend.settings')
 
 application = get_wsgi_application()
+app = application
