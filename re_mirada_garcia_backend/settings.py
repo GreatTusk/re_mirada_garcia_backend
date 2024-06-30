2f240f8c186892c7472989a5fcccb654a2796faa
F776 <fern.belmar@duocuc.cl>
2024-06-29 23:50:21 -0400

    tratar de deploy

--- re_mirada_garcia_backend/settings.py
+++ re_mirada_garcia_backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-t^)r0=5w1ozaw*5yp0-)f!jh!-&y=50hvion*+@dkx7-cy-kej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["remiradagarciabackend-production.up.railway.app"]
 
 # Application definition
 
